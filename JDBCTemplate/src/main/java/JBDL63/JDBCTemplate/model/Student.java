c1d075adc61c27321e83d1dbfb658cd5c8d5a7f2
heyChandan <chandankumarswain404@gmail.com>
2024-03-15 18:57:57 +0530

    V6.1: Validation implemented,(for field- department)PAN NO VAllidation

--- JDBCTemplate/src/main/java/JBDL63/JDBCTemplate/model/Student.java
+++ JDBCTemplate/src/main/java/JBDL63/JDBCTemplate/model/Student.java
@@ -1,6 +1,8 @@
 package JBDL63.JDBCTemplate.model;
 
 import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 import lombok.extern.slf4j.Slf4j;
 
@@ -15,8 +17,10 @@ public class Student {
 
     private Integer id;
 
+    @NotBlank(message="Student name should not be null")
     private String name;
 
+    @Pattern(regexp = "[A-Z]{5}[0-9]{4}[A-Z]{1}", message = "PAN No is not validated")
     private String department;
 
     @DecimalMin(value="60",message = "Minimum marks required is 60")
