9ae6635edcc096f35409d5fa370bda92e8071878
heyChandan <chandankumarswain404@gmail.com>
2024-03-16 18:20:22 +0530

    v1: created, using Hibernate(JPA Vendor) to perform DDL

---
+++ JPAImpl1/src/main/java/JPAImpl1/JPAImpl1/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package JPAImpl1.JPAImpl1.repository;
+
+import JPAImpl1.JPAImpl1.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Integer> {
+}
