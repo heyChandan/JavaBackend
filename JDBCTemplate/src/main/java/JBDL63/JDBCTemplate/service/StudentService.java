af4d946b0125b80a3eff673eb2d61b9d3ee4ec96
heyChandan <chandankumarswain404@gmail.com>
2024-03-18 16:22:30 +0530

    Just comment out the command for creating Student table again

--- JDBCTemplate/src/main/java/JBDL63/JDBCTemplate/service/StudentService.java
+++ JDBCTemplate/src/main/java/JBDL63/JDBCTemplate/service/StudentService.java
@@ -5,9 +5,7 @@ import JBDL63.JDBCTemplate.repository.StudentRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
