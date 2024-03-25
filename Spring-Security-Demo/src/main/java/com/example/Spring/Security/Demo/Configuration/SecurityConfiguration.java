a1cd5bad26876baff759065ddc353229a9f09032
heyChandan <chandankumarswain404@gmail.com>
2024-03-25 18:11:59 +0530

    Spring security implemented for Authentication and Authorization(admin, user)

---
+++ Spring-Security-Demo/src/main/java/com/example/Spring/Security/Demo/Configuration/SecurityConfiguration.java
@@ -0,0 +1,59 @@
+package com.example.Spring.Security.Demo.Configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    //Authentication
+    @Bean
+    public UserDetailsService getUserDetailsService(PasswordEncoder passwordEncoder){
+        UserDetails admin = User.withUsername("Chandan")
+                .password(passwordEncoder.encode("12345"))
+                .roles("ADMIN")
+                .build();
+
+        UserDetails user = User.withUsername("Jyoti")
+                .password(passwordEncoder.encode("12"))
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+
+    //Encryption
+
+    @Bean
+    public PasswordEncoder getPassWordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+
+    //Authorization
+    @Bean
+    public SecurityFilterChain getSecurityFilterChain(HttpSecurity http) throws Exception{
+        return http
+                .csrf( csrf-> csrf.disable())
+                .authorizeHttpRequests(
+                        auth ->
+                                auth.requestMatchers("/users/allowedToAll").permitAll()
+                                .requestMatchers("/users/allowedToAdmin").hasRole("ADMIN")
+                                .requestMatchers("/users/allowedToUser").hasRole("USER")
+                )
+                .formLogin(Customizer.withDefaults())
+                .build();
+    }
+}
---
+++ Spring-Security-Demo/src/main/java/com/example/Spring/Security/Demo/Controller/SecurityController.java
@@ -0,0 +1,24 @@
+package com.example.Spring.Security.Demo.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class SecurityController {
+
+    @GetMapping("/allowedToAll")
+    public String getString(){
+        return "Allowed To All";
+    }
+    @GetMapping("/allowedToAdmin")
+    public String getAdminString(){
+        return "Allowed To Admin Only";
+    }
+    @GetMapping("/allowedToUser")
+    public String getUserString(){
+        return "Allowed To User Only";
+    }
+
+}
