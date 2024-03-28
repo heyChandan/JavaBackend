f6b61b4f338124a91020e5611f610f2b80f0c80f
heyChandan <chandankumarswain404@gmail.com>
2024-03-28 16:01:56 +0530

    Kafka implemented.

---
+++ apacheKafkaDemoProducer/src/main/java/com/apacheKafkaProducer/apacheKafkaDemoProducer/ApacheKafkaDemoProducerApplication.java
@@ -0,0 +1,13 @@
+package com.apacheKafkaProducer.apacheKafkaDemoProducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApacheKafkaDemoProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApacheKafkaDemoProducerApplication.class, args);
+	}
+
+}
---
+++ apacheKafkaDemoProducer/src/test/java/com/apacheKafkaProducer/apacheKafkaDemoProducer/ApacheKafkaDemoProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apacheKafkaProducer.apacheKafkaDemoProducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApacheKafkaDemoProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
